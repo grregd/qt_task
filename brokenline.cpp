56ecc72a3c86a67517aa7603fde7dfe6c8347a54
Grzegorz Dulewicz <grregd@gmail.com>
2019-10-11 13:17:36 +0200

    wip

--- brokenline.cpp
+++ brokenline.cpp
@@ -1,7 +1,51 @@
 
 #include "brokenline.h"
 
+#include <algorithm>
+
+#include <QtDebug>
 #include <QLineF>
+
+QVector<qreal> calculateLength1(const QVector<QPoint> & points)
+{
+    QVector<qreal> result;
+
+    if (points.empty())
+    {
+        return result;
+    }
+
+    result.reserve(points.size());
+
+    QPoint prevPoint(*points.begin());
+    qreal accLen = 0;
+
+    std::transform(points.begin(), points.end(), std::back_inserter(result),
+        [&prevPoint, &accLen](auto const & point)
+        {
+            accLen += QLineF(prevPoint, point).length();
+            prevPoint = point;
+            return accLen;
+        }
+    );
+
+    return result;
+}
+
+qreal calculateLength(const QVector<QPoint> & points)
+{
+    qreal result = 0;
+
+    if (points.size() >= 2)
+    {
+        for (size_t i = 0; i < points.size()-1; ++i)
+        {
+            result += QLineF(points[i], points[i+1]).length();
+        }
+    }
+
+    return result;
+}
 
 BrokenLine & BrokenLine::addPointAtEnd(const QPoint & point)
 {
@@ -23,6 +67,10 @@ BrokenLine & BrokenLine::removePoint(const QPoint & point)
                     points_.end(),
                     point),
         points_.end());
+
+    length_ = calculateLength(points_);
+    qDebug() << "length: " << length();
+    calculateLength1(points_);
 
     return *this;
 }
