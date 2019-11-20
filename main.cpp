8d4c72fdc43eac199a8c0e0224a80b8d0d078ecf
Grzegorz Dulewicz <grregd@gmail.com>
2019-11-20 12:02:35 +0100

    Provide organization name to avoid QSettings warnings

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+    app.setOrganizationName("GradientToy Company");
 
     qmlRegisterType<GradientTool>("GradientTool", 1, 0, "GradientTool");
 
